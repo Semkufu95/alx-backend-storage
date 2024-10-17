2b17203cf6bac8e3a88c373fbbd4d561a4306186
Semkufu95 <semkufu95@gmail.com>
2024-10-17 18:03:14 +0000

    Unique id

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,10 @@
 -- A script that creates a table 'users' with attributes name, email and id
 -- The script should be able to be executed on any database and shouldn't fail
 
-CREATE TABLE users (
-UNIQUE id int NOT NULL AUTO_INCREMENT,
-UNIQUE email varchar(255) NOT NULL,
-UNIQUE name varchar(255),
-PRIMARY KEY (id),
-UNIQUE INDEX (email),
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL,
+       name VARCHAR(255),
+       PRIMARY KEY (id),
+       UNIQUE INDEX (email),
 );
